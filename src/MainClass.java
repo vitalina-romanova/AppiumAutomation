62b15a4ebc0fc7c6f9541a02643f6a27dbb8d317
Vitalina <vitalinaromashka@mail.ru>
2021-06-02 13:07:10 +0300

    test

---
+++ src/MainClass.java
@@ -0,0 +1,19 @@
+
+
+public class MainClass {
+    private static final Integer CLASS_NUMBER = 20;
+    private static final String CLASS_STRING = "Hello, world";
+
+    public int getLocalNumber(){
+         int number = 14;
+         return number;
+    }
+
+    public int getClassNumber(){
+        return CLASS_NUMBER;
+    }
+
+    public String getClassString(){
+        return CLASS_STRING;
+    }
+}
